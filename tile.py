6a0539ee422fefed9aee4090e6dc5eba6ac0b5d3
Rlapham <Rlapham@gmail.com>
2017-12-06 22:17:43 -0500

    tile

--- tile.py
+++ tile.py
@@ -1,24 +1,23 @@
 import moviepy.editor as mp
 from moviepy.editor import VideoFileClip, clips_array, vfx
 
-video = mp.VideoFileClip("files/N3.mov")
+video = mp.VideoFileClip("files/N5.mov")
 
 
 clips = []
-finalHold2 = []
+finalHold = []
 arrayHolder = []
 arrayHolder2 = []
 arrayHolder3 = []
 #
 #
-first1 = video.set_pos((0, 0)).resize((720, 360))
+first1 = video.set_pos((0, 0)).resize((640, 320))
 first2 = first1.fx( vfx.mirror_x)
 first3 = first1.fx( vfx.mirror_y)
 first4 = first2.fx( vfx.mirror_y)
-finalHold2 = clips_array([[first1, first2],
+finalHold = clips_array([[first1, first2],
                          [first3, first4]])
 
 
-
 # composition = mp.concatenate(clips)
-finalHold2.write_videofile("files/neurontest.mp4")
+finalHold.write_videofile("files/N5_K.mp4")
